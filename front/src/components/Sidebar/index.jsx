e86195643bb177b142b7c1b8fb2f81913470a129
Mateus Satoh <mateussatoh@gmail.com>
2021-07-20 14:57:59 -0300

    add: common components

--- front/src/components/Sidebar/index.jsx
+++ front/src/components/Sidebar/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-import {Container} from './styles'
+import { Container, GroupList, GroupContainer, GroupName } from './styles'
+import { SignIn } from 'phosphor-react'
+
+import Logo from '../Common/Logo'
 
 export default function Sidebar() {
   return (
     <Container>
+      <Logo />
+      <GroupList>
+        <GroupContainer>
+        <GroupName>Pack</GroupName>
+        <SignIn/>
+        </GroupContainer>
+      </GroupList> 
     </Container>
   )
 }
--- front/src/components/Sidebar/styles.js
+++ front/src/components/Sidebar/styles.js
@@ -2,10 +2,23 @@ import styled from "styled-components";
 import theme from "../../global/theme";
 
 const Container = styled.div`
+  display:'flex';
   background-color: ${theme.colors.alterdataBlue};
   margin-top: -100px;
   width: 280px;
   height: 100%;
 `
 
-export {Container}
+const GroupList = styled.div`
+  display:'flex';
+`
+
+const GroupContainer = styled.div`
+  display: 'flex';
+`
+
+const GroupName = styled.span`
+  display:'flex';
+`
+
+export {Container, GroupList,GroupContainer, GroupName}
