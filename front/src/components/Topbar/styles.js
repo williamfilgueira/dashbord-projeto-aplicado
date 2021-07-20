537d9ed5e106956457789b4157e13b2199fb3673
Mateus Satoh <mateussatoh@gmail.com>
2021-07-20 15:48:58 -0300

    add: common input styles

--- front/src/components/Topbar/styles.js
+++ front/src/components/Topbar/styles.js
@@ -3,8 +3,10 @@ import theme from "../../global/theme";
 
 const Container = styled.div`
     display: flex;
+    align-items: center;
     background-color: ${theme.colors.alterdataBlue};
     height: 100px;
+    padding-left: 325px;
 `
 
 export {Container}; 
