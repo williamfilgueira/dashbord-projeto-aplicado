88b408c350631599ec39f35f0450f00ce22fa9be
Lyllys Galhardo <lyllysgalhardo@gmail.com>
2021-07-20 11:43:13 -0300

    Padrão de projeto

---
+++ front/src/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    Switch,
+    Route,
+} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
+
+export default function Routes() {
+    return (
+       
+            <Switch>
+                <Route exact path="/">
+                    <Dashboard />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/signup">
+                    <Signup />
+                </Route>
+                <Route path="*">
+                    <NoMatch />
+                </Route>
+            </Switch>
+        
+
+    )
+}; 
