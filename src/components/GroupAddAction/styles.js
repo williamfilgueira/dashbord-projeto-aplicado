6d864efc8a3e4502a066da9930c066c8bf02e874
Mateus Satoh <mateussatoh@gmail.com>
2021-07-28 15:56:01 -0300

    add: new actions hover

--- src/components/GroupAddAction/styles.js
+++ src/components/GroupAddAction/styles.js
@@ -2,13 +2,15 @@ import styled from "styled-components";
 import theme from "../../global/theme";
 
 const ActionContainer = styled.div`
+  width: 100%;
   margin: 10px 0 10px 0;
   display: flex;
   justify-content: space-between;
   cursor: pointer;
-  padding: 10px 10px 10px 5px;
-  border-radius: 3px;
-  &:hover{
+  padding: 10px 10px 10px 10px;
+  border-radius: 6px;
+
+  &:hover {
     background-color: ${theme.colors.alterdataSecondaryBlue};
   }
 `;
--- src/components/Topbar/styles.js
+++ src/components/Topbar/styles.js
@@ -3,13 +3,13 @@ import theme from "../../global/theme";
 
 const Container = styled.div`
   position: fixed;
-  padding-left: 35px;
+  padding-left: 30px;
   top: 0;
   width: 100vw;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   background-color: ${theme.colors.alterdataBlue};
   height: 100px;
 `;
