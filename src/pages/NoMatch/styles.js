34be6aabd8c6aca4be44b67bbb79f84a94cf4455
Lyllys Galhardo <lyllysgalhardo@gmail.com>
2021-08-03 21:23:59 -0300

    Responsividade página 404

--- src/pages/NoMatch/styles.js
+++ src/pages/NoMatch/styles.js
@@ -4,23 +4,37 @@ import theme from "../../global/theme";
 
 const Imagem = styled.img`
   width: 600px;
+
+  @media screen and (max-width: 600px) {
+    width: 350px;
+  }
 `;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 100px auto 10px auto;
+  margin: 100px 0 10px 0;
+  @media screen and (max-width: 600px) {
+   width: 100%;
+   height: 100vh;
+   margin:0;
+   justify-content: center;
+  }
 `;
 const Title = styled.h1`
   font-family: ${theme.fonts.paragraph};
   color: ${theme.colors.darkerGray};
-  margin: 10px auto 10px auto;
+  margin: 10px 0 10px 0;
+  text-align: center;
+  font-weight: 300;
 `;
 const Subtitle = styled.h2`
   font-family: ${theme.fonts.paragraph};
   font-size: 1.2rem;
   color: ${theme.colors.darkerGray};
   margin: 10px auto 20px auto;
+  text-align: center;
+  font-weight: 400;
 `;
 
 const StyledLink = styled(Link)`
